67a70bdbe3f37e5a8f4eb6a3e463b06ce2d67a37
Volodymyr <yushkevich.v2002@gmail.com>
2024-11-26 16:47:48 +0100

    Created and edited DniCalculator.java in Model

--- src/main/java/factoria5/katadni/Model/DniCalculator.java
+++ src/main/java/factoria5/katadni/Model/DniCalculator.java
@@ -1,5 +1,21 @@
 package factoria5.katadni.Model;
 
 public class DniCalculator {
-    
+    private final char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D',
+                                   'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L',
+                                   'C', 'K', 'E'};
+
+    public boolean isValidDni(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            return number >= 0 && number <= 99999999;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public char calculateDniLetter(int dniNumber) {
+        int remainder = dniNumber % 23;
+        return letras[remainder];
+    }
 }
