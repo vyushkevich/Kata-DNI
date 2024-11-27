5a47fcb7db3cb653aa8db2d90907837b98320ed0
Volodymyr <yushkevich.v2002@gmail.com>
2024-11-27 12:11:45 +0100

    Created and edited DniController.java in Controller

--- src/main/java/factoria5/katadni/Controller/DniController.java
+++ src/main/java/factoria5/katadni/Controller/DniController.java
@@ -1,5 +1,30 @@
 package factoria5.katadni.Controller;
 
+import model.DniCalculator;
+import view.ConsoleView;
+
 public class DniController {
-    
+    private final DniCalculator model;
+    private final ConsoleView view;
+
+    public DniController(DniCalculator model, ConsoleView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void run() {
+        while (true) {
+            String input = view.promptDni();
+
+            if (!model.isValidDni(input)) {
+                view.showInvalidInputMessage();
+                continue;
+            }
+
+            int dniNumber = Integer.parseInt(input);
+            char dniLetter = model.calculateDniLetter(dniNumber);
+            view.showDniResult(dniNumber, dniLetter);
+            break;
+        }
+    }
 }
