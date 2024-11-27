80a682342a768327c2d16e7a5a0d84ef05c6db84
Volodymyr <yushkevich.v2002@gmail.com>
2024-11-27 11:34:53 +0100

    Created and edited ConsoleView.java in View

--- src/main/java/factoria5/katadni/View/ConsoleView.java
+++ src/main/java/factoria5/katadni/View/ConsoleView.java
@@ -1,5 +1,20 @@
 package factoria5.katadni.View;
 
+import java.util.Scanner;
+
 public class ConsoleView {
-    
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String promptDni() {
+        System.out.println("Introduce el número del DNI (0-99999999): ");
+        return scanner.nextLine();
+    }
+
+    public void showInvalidInputMessage() {
+        System.out.println("El dato introducido es incorrecto");
+    }
+
+    public void showDniResult(int dni, char letter) {
+        System.out.println("El NIF es: " + dni + letter);
+    }
 }
